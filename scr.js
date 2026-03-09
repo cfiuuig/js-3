1d35dcc13d28ead5d6a3cecdabca2916f21bd7ec
js-3 <js-3@gm.cm>
2026-03-09 16:10:15 +0500

    sayt

--- scr.js
+++ scr.js
@@ -1,1 +1,8 @@
-2
+var a = +prompt("количество бананов")
+if (!isNaN(a = 1)) {
+    alert(a + ' banana');
+} else if (a > 1) {
+    alert(a + 'bananas');
+} else {
+    confirm("нелзяписать юуквы")
+}
