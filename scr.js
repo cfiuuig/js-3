c290e38d4f896ef322d71d9efdefdef70f96c22d
js-3 <js-3@gm.cm>
2026-03-10 22:09:21 +0500

    привет

--- scr.js
+++ scr.js
@@ -1,8 +1,30 @@
-var a = +prompt("количество бананов")
-if (!isNaN(a = 1)) {
-    alert(a + ' banana');
-} else if (a > 1) {
-    alert(a + 'bananas');
-} else {
-    confirm("нелзяписать юуквы")
+let num;
+do {
+    num = +prompt("введите кол бананов")
+    if (isNaN(num)) {
+        alert("введите число!")
+    }
+} while (isNaN(num));
+console.log(num + (num === 1 ? " banana" : " bananas"));
+
+
+do {
+    num = +prompt("Введите число");
+
+    if (isNaN(num)) {
+        alert("Введите число!");
+    }
+
+} while (isNaN(num));
+
+let sum = 0;
+
+for (let i = 1; i <= num; i++) {
+
+    if (i % 2 === 0) {
+        sum += i;
+    }
+привет
 }
+
+alert("Сумма четных чисел = " + sum);
